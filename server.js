47c80531b7d584b5e63b294eb9a533a213add1d1
Anurag Choudhary <anurag@iface.io>
2025-05-05 00:32:18 +0530

    new deployment

--- server.js
+++ server.js
@@ -24,10 +24,15 @@ try {
 
 // Setup email transporter (Gmail)
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD // Use App Password for Gmail
+  },
+  tls: {
+    rejectUnauthorized: false // Use only in development, remove in production!
   }
 });
 
